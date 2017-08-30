623ab153c295a1293daa3777626f9b81d410e54d
Emily Deans <emilyldeans@gmail.com>
2017-08-30 14:00:07 -0400

    Created users controllers and views to allow user login and signup with unique email validation and password authorization.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email,
+   uniqueness: true,
+   format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+  before_validation :downcase_email
+
+  private
+
+  def downcase_email
+    email.downcase if email
+  end
 end
