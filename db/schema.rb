208bb1642c3a442d45666c5cf1d417ef4129b41b
Emily Deans <emilyldeans@gmail.com>
2017-08-31 13:47:47 -0400

    Updated schema to include new associations.

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20170830160241) do
     t.string "author"
     t.text "description"
     t.text "URL"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -24,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170830160241) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
